21f25d3cadf4b9962160a70afbc5985d2c6a9d7e
haafizharafs <haafizharafs@gmail.com>
2023-11-21 10:19:34 +0700

    view verifikasi dosen

--- resources/views/dosen/verifikasi.blade.php
+++ resources/views/dosen/verifikasi.blade.php
@@ -7,11 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="{{ asset('/') }}/css/styleDsn.css" />
     <title>SiHadir</title>
-    <link rel="shortcut icon" href="images/sihadir.png">
+    <link rel="shortcut icon" href="{{ asset('/') }}/images/sihadir.png">
 </head>
 
 <body>
@@ -20,19 +21,36 @@
 
         <div class="bg-side" id="sidebar-wrapper">
             <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
-                <img src="images/sihadir2.png" alt="" width="45" height="50"> SiHadir
+                <img src="{{ asset('/') }}/images/sihadir2.png" alt="" width="45" height="50"> SiHadir
 
             </div>
             <div class="list-group list-group-flush my-3">
-                <a href="dosen.html" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                        class="fas fa-home me-3"></i>Dashboard</a>
-                <a href="verifikasi.html" class="list-group-item list-group-item-action bg-transparent second-text active"><i
-                        class="fas fa-check-circle me-3"></i>Verifikasi</a>
-                <a href="jadwal.html" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                        class="fas fa-file-alt me-3"></i> Jadwal</a>
-                <a href="datarekap.html" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                        class="fas fa-calendar-alt me-3"></i> Data Rekap</a>
-                <a href="index.html" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i
+                <a href="{{ url('/dosen/dashboard') }}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                    class="fas fa-home me-3"></i>Dashboard</a>
+            <a href="{{ url('/dosen/verifikasi') }}" class="list-group-item list-group-item-action bg-transparent second-text active"><i
+                    class="fas fa-check-circle me-3"></i>Verifikasi</a>
+            <button class="list-group-item list-group-item-action bg-transparent second-text fw-bold d-block"
+                onclick="myAccFunc()"><i class="fas fa-file-alt me-3"></i>
+                Jadwal <i class=" fa fa-caret-down"></i>
+            </button>
+            <div id="demoAcc" class=" d-none   ">
+                <a href="#" class=" d-flex flex-column w3-bar-item w3-button">Jadwal</a>
+                <a href="#" class="d-flex flex-column w3-bar-item w3-button">Kelas</a>
+                <a href="#" class="d-flex flex-column w3-bar-item w3-button">Semester</a>
+                <a href="#" class="d-flex flex-column w3-bar-item w3-button">Hari</a>
+                <a href="#" class="d-flex flex-column w3-bar-item w3-button">Jam</a>
+
+            </div>
+            <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                    class="fas fa-calendar-alt me-3"></i> Data Rekap</a>
+                <!--form untuk logout-->
+                <form id="logout-form" action="{{ route('logout') }}" method="post">
+                    @csrf
+                </form>
+                <!--tombol logout yang mengakses form logout-->
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                    class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i
                         class="fas fa-power-off me-2"></i>Logout</a>
             </div>
         </div>
@@ -62,7 +80,8 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
                             role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="fas fa-user me-2"></i>&nbsp &nbsp &nbsp &nbsp Dosen
+                            <i class="fas fa-user me-2"></i>&nbsp &nbsp &nbsp &nbsp
+                            &nbsp{{ Auth::user()->dosen->nama }}
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="profile.html">Profile</a></li>
@@ -88,7 +107,7 @@
     <!-- /#page-content-wrapper -->
 
 
-    <script src="main.js"></script>
+    <script src="{{ asset('/') }}/js/mainDsn.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> -->
     <script>
@@ -105,6 +124,32 @@
         toggleButton.onclick = function () {
             el.classList.toggle("toggled");
         };
+    </script>
+    {{-- toogle --}}
+    <script>
+        function myAccFunc() {
+            var x = document.getElementById("demoAcc");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+                x.previousElementSibling.className += " w3-green";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+                x.previousElementSibling.className =
+                    x.previousElementSibling.className.replace(" w3-green", "");
+            }
+        }
+
+        function myDropFunc() {
+            var x = document.getElementById("demoDrop");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+                x.previousElementSibling.className += " w3-green";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+                x.previousElementSibling.className =
+                    x.previousElementSibling.className.replace(" w3-green", "");
+            }
+        }
     </script>
 </body>
 
